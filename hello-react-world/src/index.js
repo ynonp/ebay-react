a793e8799eef7c14a5e3abdd8e49edb7ad9e099c
ynonp <ynonperek@gmail.com>
2019-04-01 10:46:43 +0300

    first example yay

---
+++ hello-react-world/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+function App(props) {
+  return <h1>Hello World</h1>;
+}
+
+ReactDOM.render(<App />, document.querySelector('main'));
