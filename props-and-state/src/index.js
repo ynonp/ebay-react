9e1a2d5dc3227ca98a1a02d4016d1cf63c17fbb1
ynonp <ynonperek@gmail.com>
2019-04-01 12:07:26 +0300

    add lab

--- props-and-state/src/index.js
+++ props-and-state/src/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Clicker from './clicker';
 import Indicator from './indicator';
+import ClickerWithIndicator from './clicker_with_indicator';
 
 // same as:
 // const useState = React.useState;
@@ -11,6 +12,8 @@ function App(props) {
   return (
     <div>
       <h1>Hello World</h1>
+      <ClickerWithIndicator />
+
       <Clicker />
       <Indicator value={91} />
 
