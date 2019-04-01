3c305605ab2616dfa3d4e58bfd00515b124d0248
ynonp <ynonperek@gmail.com>
2019-04-01 15:17:29 +0300

    solved lab time converter

--- lab-time-converter/src/index.js
+++ lab-time-converter/src/index.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 function TimeConverter(props) {
+  const [ seconds, setSeconds ] = useState(0);
+
+  const minutes = seconds / 60;
+  const hours = seconds / 3600;
+
+  const makeChangeHandler = (multiplier) => (e) => {
+    setSeconds(e.target.value * multiplier);
+  }
+
+  /*
+   * Same as:
+  function makeChangeHandler(multiplier) {
+    return function(e) {
+      setSeconds(e.target.value * multiplier);
+    }
+  }
+  */
+
   return (
     <div className='time-converter'>
       <label>
-        Seconds: <input type='text' />
+        Seconds: <input type='text' value={seconds} onChange={makeChangeHandler(1)} />
       </label>
 
       <label>
-        Minutes: <input type='text' />
+        Minutes: <input type='text' value={minutes} onChange={makeChangeHandler(60)} />
       </label>
 
       <label>
-        Hours: <input type='text' />
+        Hours: <input type='text' value={hours} onChange={makeChangeHandler(3600)} />
       </label>
     </div>
   )
